e345ab68566109d3f914c355bc9add4655361554
momokonomi <momokonomipaint@gmail.com>
2024-12-29 06:46:16 +0000

    feat: #1 入力フォームにイベント追加

--- src/libs/domUnit.js
+++ src/libs/domUnit.js
@@ -2,14 +2,15 @@
  * Json上のdom定義から単一のDOM Elementを作成する
  */
 export default function domUnit(domJson) {
-  const dom = document.createElement(domJson.tag);
+  const dom = document.createElement(String(domJson.tag));
   dom.innerHTML = domJson.innerHTML ?? '';
   //属性追加
   for (const href of Object.keys(domJson.href)) {
-    if (!dom[href]) {
+    if (!domJson.href[href]) {
       continue;
     }
-    dom[href] = domJson[href];
+    dom.setAttribute(href, domJson.href[href]);
   }
+
   return dom;
 }
